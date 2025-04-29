08c1d4ff4bceb96daf7743aba694df25f3a6ec5e
Maryna <89296066105marina@gmal.com>
2025-04-29 17:32:56 +0200

    start8

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 
 import './App.css'
-import Header from './lesson_1/components/Header/Header'
-import TodoList from './lesson_1/components/TodoList/TodoList'
-import Practic from './Practic/practic'
 
 function App() {
   return(<>
-  <Header/>
-  <TodoList/>
+
   </>)
 }
 
